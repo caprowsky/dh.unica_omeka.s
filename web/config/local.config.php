3054d6d4e9dd7403bffba4ba1914df1643e41e11
Alessandro Capra <alessandrocapra@gmail.com>
2025-02-08 19:04:59 +0100

    Aggiungo le config per invio SMTP

--- web/config/local.config.php
+++ web/config/local.config.php
@@ -1,8 +1,8 @@
 <?php
 return [
     'logger' => [
-        'log' => true,
-        'priority' => \Zend\Log\Logger::DEBUG,
+        'log' => false,
+        'priority' => \Laminas\Log\Logger::NOTICE,
     ],
     'http_client' => [
         'sslcapath' => null,
@@ -28,6 +28,23 @@ return [
         'aliases' => [
             'Omeka\File\Store' => 'Omeka\File\Store\Local',
             'Omeka\File\Thumbnailer' => 'Omeka\File\Thumbnailer\ImageMagick',
+        ],
+    ],
+    'mail' => [
+        'transport' => [
+            'type' => 'smtp',
+            'options' => [
+                'name' => 'smtp.unica.it',
+                'host' => 'smtp.unica.it',
+                'port' => 25, // 465 for 'ssl', and 587 for 'tls'
+                'connection_class' => 'smtp', // 'plain', 'login', or 'crammd5'
+                'connection_config' => [
+                    'username' => null,
+                    'password' => null,
+                    'ssl' => null, // 'ssl' or 'tls'
+                    'use_complete_quit' => true,
+                ],
+            ],
         ],
     ],
 ];
